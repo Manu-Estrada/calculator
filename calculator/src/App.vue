fa887b4fa0ce1595bcf27c95c9be3959f4cf9bd8
Manu-Estrada <mirucuxusticieru@gmail.com>
2022-12-30 19:58:24 +0100

    comiteando

---
+++ calculator/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <calculator />
+  </div>
+</template>
+
+<script >
+import calculator from "./components/calculator.vue"
+
+export default {
+  name: "app",
+  components: {
+    calculator
+  }
+}
+</script>
+
+
+<style>
+
+</style>
